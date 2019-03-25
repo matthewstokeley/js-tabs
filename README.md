73092bfb781b5becc40ec0cb76b5386bad2711ab
MacbookPro <macbookpro@MacBook-Pro.local>
2019-03-25 11:43:17 -0400

    init

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 0.0.1
 
-I've written this with a revealing module pattern instead of a es6 class in order to emulate the jQuery api - I am currently reconsidering. 
+
+### js-tabs
--- index.js
+++ index.js
@@ -1,56 +1,87 @@
+/**
+ * query-like api for handling tabs
+ *
+ * @param 
+ * @param
+ * @param    {Object}
+ * @returns  {Object}
+ */
 const tabs = function(el, data, options) {
-	if (!options) {
-		const options = {
-			toggleMethod: 'display'
-		}
-	}
 
-	const name = data.tab
-	const container = query('#menu a').element
+    if (!options) {
+        const options = {
+            toggleMethod: 'display'
+            datasetNames: {}
+        }
+    }
 
-	const selectTabByName = function(name) {
-		return document.getElementById(name)
-	}
+    const name = data.tab
+    const container = query('#menu a').element
 
-	const closeExcept = function(name) {
-		try {
-			for (let i = 0; i < container.length; i++) {
-				if (container[i].dataset.tab !== name) {
-					const tab = selectTabByName(container[i].dataset.tab)
-					hide(tab)
-				}
-			}
-		} catch(e) {
-			throw new Error(e)
-		}
-	}
+    /**
+     * [_selectTabByName description]
+     *
+     * @var      {String}
+     * @private
+     * @return   {Node}
+     */
+    const _selectTabByName = function(name: String) {
+        return document.getElementById(name)
+    }
 
-	const show = function(tab) {
-		if (!tab) {
-			throw new Error('invalid argument')
-		}
-		tab.style.display = 'block'
-	}
 
-	const hide = function(tab) {
-		if (!tab) {
-			throw new Error('invalid argument')
-		}
+    const _show = function(tab: String) {
+        if (!tab) {
+            throw new Error('invalid argument')
+        }
+        tab.style.display = 'block'
+        return this
+    }
 
-		tab.style.display = 'none'
-	}
+    const _hide = function(tab: String) {
+        if (!tab) {
+            throw new Error('invalid argument')
+        }
 
-	const open = function() {
-		try { 
-    		closeExcept(name)
-    		show(document.getElementById(name))
-    	} catch(e) {
-    		throw new Error(e)
-    	}
-	}
-	
-	return {
-		open: open,
-		closeExcept: closeExcept
-	}
+        tab.style.display = 'none'
+        return this
+    }
+
+    const closeExcept = function(name) {
+        try {
+            for (let i = 0; i < container.length; i++) {
+                if (container[i].dataset.tab !== name) {
+                    const tab = _selectTabByName(container[i].dataset.tab)
+                    hide(tab)
+                }
+            }
+        } catch (e) {
+            throw new Error(e)
+        }
+    }
+
+    const open = () => {
+        try {
+            closeExcept(name)
+            _show(document.getElementById(name))
+        } catch (e) {
+            throw new Error(e)
+        }
+    }
+
+    const close = () => {
+        try {
+            _hide(document.getElementById(name))
+        } catch (e) {
+            throw new Error(e)
+        }
+    }
+
+    const closeAll = () => {}
+
+    return {
+        open: open,
+        closeExcept: closeExcept,
+        close: close
+    }
 }
